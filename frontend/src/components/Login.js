d492c98f15e92bcd17a5963a8f8ab0d75669330d
Nick Reno <nick.p.reno@gmail.com>
2021-03-06 16:41:09 -0500

    Login Part 1
    This is so Emily can have access to the updated Navbar.  My Login page is in the works.

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,14 +1,30 @@
-//Am I supposed to import bootstrap here so that I can use container, row, and col?
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Breadcrumb, Form } from "react-bootstrap";
 
 const Login = () => {
     return (
       <div className="LoginClassName">
           <Container>
-              <Row>
-                  <Col><p style={{color: "white"}}>Login Component</p></Col>
-                  <Col><p style={{color: "white"}}>Sign Up Component</p></Col>
+              <Form>
+                  <Form.Group>
+                      <Form.Label>Username/Email</Form.Label>
+                      <Form.Control type="email" placeholder="GuysI'mActuallyDoingIt" />
+                      <Form.Text className="text-muted" >
+                          We have your feetprints ;-;
+                      </Form.Text>
+                  </Form.Group>
+              </Form>
+              <Row className="TopLoginRow">
+                  <Col className="LeftLoginCol">
+                      <p style={{color: "orange"}}>Login Component</p>
+
+                  </Col>
+                  <Col className="RightLoginCol">
+                      <p style={{color: "blue"}}>Sign Up Component</p>
+                  </Col>
               </Row>
+              <Breadcrumb className="justify-content-center">
+                  <Breadcrumb.Item>Test</Breadcrumb.Item>
+              </Breadcrumb>
           </Container>
       </div>
     );
