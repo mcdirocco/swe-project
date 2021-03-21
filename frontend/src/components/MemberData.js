330e9b31932df66b04c45aa77499b3a4f1e4a827
Nick Reno <nick.p.reno@gmail.com>
2021-03-20 20:32:00 -0400

    View Member Data Update
    Partial Scroll wheel, no search bar, right side card.

--- frontend/src/components/MemberData.js
+++ frontend/src/components/MemberData.js
@@ -4,11 +4,11 @@ import "./MemberData.css";
 const MemData = () => {
     return (
       <div className="ClassNameBigDiv">
-        <header className="HeaderClassName">
-          Nick is now working on this page.
+        <header className="HeaderClassName" >
+          Active Members
         </header>
           <Container>
-              <Row>
+              <Row className="RowButtons">
                   <Col>
                       <Button variant="primary" className="btn btn-block">Members</Button>
                   </Col>
@@ -16,7 +16,40 @@ const MemData = () => {
                       <Button variant="primary" className="btn btn-block">Events</Button>
                   </Col>
               </Row>
-
+              <Row className="RowSplitPage">
+                  <Col>
+                    <Form>
+                        This side is for the search bar, Scroll wheel, and Member mini-preview/title tags
+                    </Form>
+                    <Form.Control type="formBasicText" placeholder="This is where we put the search bar. (Replace this)" />
+                      <div className="form-group">
+                          <label htmlFor="exampleFormControlSelect2">Example multiple select</label>
+                          <select multiple className="form-control" id="exampleFormControlSelect2">
+                              <option>1</option>
+                              <option>2</option>
+                              <option>3</option>
+                              <option>Will we sort this alphabetically or by membership, points, attendance, or have ways to sort it?</option>
+                              <option>4</option>
+                              <option>Need a way to add in a member automatically once they make an account</option>
+                          </select>
+                      </div>
+                  </Col>
+                  <Col>
+                    <Form>
+                        This side is for all the available information allowed to see by different users
+                    </Form>
+                      <div className="card">
+                          <div className="card-header">
+                              Member's First and Last name
+                          </div>
+                          <div className="card-body">
+                              <h5 className="card-title" align="left">Username: <br /> Points: <br /> Tier Level: <br /> Attendance Level: </h5>
+                              <p className="card-text">Show personal biography here? Large area for information.</p>
+                              <a href="#" className="btn btn-primary">Go somewhere</a>
+                          </div>
+                      </div>
+                  </Col>
+              </Row>
           </Container>
       </div>
     );
