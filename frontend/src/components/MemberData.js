5af43eb6f715cff30cea1ed191d7dfb2a97217a0
Nick Reno <nick.p.reno@gmail.com>
2021-03-22 16:17:01 -0400

    ViewMemData Update
    Minor updates for formatting.

--- frontend/src/components/MemberData.js
+++ frontend/src/components/MemberData.js
@@ -4,42 +4,47 @@ import "./MemberData.css";
 const MemData = () => {
     return (
       <div className="ClassNameBigDiv">
-        <header className="HeaderClassName" >
+        <header className="HeaderClassName">
           Active Members
         </header>
           <Container>
               <Row className="RowButtons">
                   <Col>
-                      <Button variant="primary" className="btn btn-sm">Members</Button>
+                      <Button variant="primary" className="btn btn-lg btn-dope" style={{span: 3, offset: 5}}>Members</Button>
                   </Col>
                   <Col>
-                      <Button variant="primary" className="btn btn-sm" aria-pressed="true">Events</Button>
+                      <Button variant="primary" className="btn btn-lg btn-dope" aria-pressed="true">Events</Button>
                   </Col>
               </Row>
               <Row className="RowSplitPage">
-                  <Col>
-                    <Form>
-                        This side is for the search bar, Scroll wheel, and Member mini-preview/title tags
-                    </Form>
+                  <Col >            {/*style={{span: 6, offset: 4}}*/}
+                      {/*<div className="card">*/}
+                      {/*    <div className="card-header">*/}
+                      {/*        <Form>*/}
+                      {/*            This side is for the search bar, Scroll wheel, and Member mini-preview/title tags*/}
+                      {/*        </Form>*/}
+                      {/*    </div>*/}
+
                       <Form className="form-inline my-2 my-lg-0">
                           <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
                               <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                       </Form>
                       <div className="form-group">
-                          <label htmlFor="exampleFormControlSelect2">Example multiple select</label>
+                          <label htmlFor="exampleFormControlSelect2" style={{color: "white"}}>Example multiple select</label>
                           <select multiple className="form-control" id="exampleFormControlSelect2">
                               <option>1</option>
-                              <option>Need a way to add in a member automatically once they make an account</option>
+                              {/*<option>Need a way to add in a member automatically once they make an account</option>*/}
+                              <option>2</option>
+                              {/*<option>Will we sort this alphabetically or by membership, points, attendance, or have ways to sort it?</option>*/}
                               <option>3</option>
-                              <option>Will we sort this alphabetically or by membership, points, attendance, or have ways to sort it?</option>
-                              <option>4</option>
                           </select>
                       </div>
+                      {/*</div>*/}
                   </Col>
                   <Col>
-                    <Form>
-                        This side is for all the available information allowed to see by different users
-                    </Form>
+                    {/*<Form>*/}
+                    {/*    This side is for all the available information allowed to see by different users*/}
+                    {/*</Form>*/}
                       <div className="card">
                           <div className="card-header">
                               Member's First and Last name
